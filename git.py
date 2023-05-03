7da0b47a280f72f1c9b49b3606ac5c7e91ef3c79
AnjaliChapagain <anjaliichz@gmail.com>
2023-05-03 15:47:22 +0545

    commit message

--- git.py
+++ git.py
@@ -1,9 +1,18 @@
 # git init
-# git add .
+# git add . (git add --all)
 # git commit -m "commit_message>"  ---- determines who wrote the code
+# git remote add origin <ssh link>
+# git push -u origin <branch_name>
+
 
 # git status # Shows the current status of all git staged and unstaged files
 
-# setting git configuration
+# setting git configuration  -- don't have to do this everytime, already done
 # 1. git config --global user.name "<username>"
 # 2. git config --global user.email "<your-email>"
+
+# Generating ssh key pair ----don't have to do this everytime, already done
+# ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
+
+# git branch # It shows all the branches and highlighjts the current branch
+
